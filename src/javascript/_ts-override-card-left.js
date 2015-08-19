29d12728a788d6b4067fb937802526fd8d7413c6
John Martin <curmudgeon+git@rallydev.com>
2015-08-19 11:41:40 -0700

    fix missing discussion/task summaries

--- src/javascript/_ts-override-card-left.js
+++ src/javascript/_ts-override-card-left.js
@@ -31,13 +31,12 @@
 Ext.override(Rally.ui.cardboard.plugin.CardContentLeft,{
 
     _getRenderTpl: function(fieldDefinition) {
-        console.log('_getRenderTpl',fieldDefinition);
-        
         var card = this.card,
             modelField = card.getRecord().getField(fieldDefinition.name),
             hasData = true /*(Ext.isFunction(fieldDefinition.hasValue) && fieldDefinition.hasValue()) || card.getRecord().hasValue(modelField)*/,
             isRenderable = hasData || (modelField && modelField.isCollection());
 
+            
         if (modelField && modelField.isHidden) {
             return null;
         }
@@ -53,6 +52,15 @@ Ext.override(Rally.ui.cardboard.plugin.CardContentLeft,{
         return fieldDefinition.renderTpl;
     },
 
+    _hasData: function(card,fieldDefinition) {
+        var record = card.getRecord();
+        var modelField = card.getRecord().getField(fieldDefinition.name);
+
+        return (Ext.isFunction(fieldDefinition.hasValue) && fieldDefinition.hasValue()) 
+            || record.hasValue(modelField)
+            || ( record.get("Summary")[fieldDefinition.name] && record.get("Summary")[fieldDefinition.name].Count > 0 ) // collections;
+    },
+    
     _getFieldHtml: function(fieldDefinition) {
         var html = '',
             cls = '',
@@ -60,12 +68,15 @@ Ext.override(Rally.ui.cardboard.plugin.CardContentLeft,{
             tpl = this._getRenderTpl(fieldDefinition);
 
         if (tpl) {
-            html = tpl.apply(this.card.getRecord().data);
             var card = this.card;
+            html = tpl.apply(card.getRecord().data);
+            // 
+            
             
             // don't show collections unless there's a value
             var modelField = card.getRecord().getField(fieldDefinition.name);
-            var hasData = (Ext.isFunction(fieldDefinition.hasValue) && fieldDefinition.hasValue()) || card.getRecord().hasValue(modelField);
+            var hasData = this._hasData(card,fieldDefinition);
+            
             if ( modelField && modelField.isCollection() && !hasData ) {
                 return '';
             }
